34ea14c573d53716771b67c5b0f3ae420a65b44e
kurihara119 <yoshiki.k.0119@gmail.com>
2023-03-02 22:14:13 +0900

    画像表示設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,13 +6,13 @@ CarrierWave.configure do |config|
   if Rails.env.production? # 本番環境の場合はS3へアップロード
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'animerecord-bucket'
+    config.fog_directory =  ENV['S3_BUCKET']
     config.fog_public = false
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1',
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       path_style: true
     }
   else # 本番環境以外の場合はアプリケーション内にアップロード
